c4fb667084b6cd85c8c81a163e39fe9795e018ad
Peter Kong <peter.kong@lmnsolutions.com>
2011-07-29 00:50:37 -0400

    added load_tweets script, initialized db

---
+++ script/load_tweets.rb
@@ -0,0 +1,8 @@
+Post.transaction do
+
+  Twitter.user_timeline.each do |tweet|
+    Post.create!Post.create(:content => tweet.text, :creator => tweet.user.values_at("screen_name").to_s)
+  end
+
+end
+
