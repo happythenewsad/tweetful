dde9d15c23ae6b96c708ad8465269f343e8c61be
Peter Kong <peter.kong@lmnsolutions.com>
2011-09-13 00:40:05 -0400

    dunno

--- app/workers/tweet_loader.rb
+++ app/workers/tweet_loader.rb
@@ -4,10 +4,9 @@ class TweetLoader
   @queue = :tweet_queue
   
   def self.perform()
-    STDERR.puts "PERFORM HAS STARTED ************"
     credentials = YAML::load(File.read(File.join(RAILS_ROOT, 'config', 'not_public', 'twitter.yml')))['twitter']
     username, password = credentials['username'], credentials['password']
-    TweetStream::Client.new(username, password).track('mac', 'lion') do |status|
+    TweetStream::Client.new(username, password).track('mac') do |status|
       STDERR.puts "a tweet from #{status.user.screen_name}"
       Post.create! do |post|
         post.creator = status.user.screen_name
