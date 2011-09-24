768484ffbffcaed7a189f3abe25e894db4607021
Peter Kong <peter.kong@lmnsolutions.com>
2011-09-24 18:40:54 -0400

    removed tweetful.rb init file

--- config/initializers/tweetful.rb
+++
@@ -1,6 +0,0 @@
-#STDERR.puts "TweetLoader initializing..."
-#Resque.enqueue(TweetLoader) #unless Resque.info[:workers] >= 1
-#STDERR.puts "#{Resque.info[:workers]} working"
-#STDERR.puts "TweetLoader initialized."
-
-STDERR.puts "starting rails, pid=#{Process.pid}"
